82c35687098273e96c9603c00c96d08ba1a692de
yosshi0127-ios <yosshi.0127.ios@gmail.com>
2023-05-04 17:25:46 +0900

    183. Adding The Add HappyPlaceActivity and using dimens, string, color

--- HappyPlaces/app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
+++ HappyPlaces/app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,21 @@ package com.example.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
+        val toolBarPlace: Toolbar = findViewById(R.id.toolbar_add_place)
+
+        // ActionBarをセット
+        setSupportActionBar(toolBarPlace)
+        // ActionBar戻るボタン追加
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolBarPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- HappyPlaces/app/src/main/java/com/example/happyplaces/MainActivity.kt
+++ HappyPlaces/app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fabAddHappyPlace:
+                FloatingActionButton = findViewById(R.id.fabAddHappyPlace)
+        fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
