45a812a654d6c21e615170011c0b317f6b5a60ce
yosshi0127-ios <yosshi.0127.ios@gmail.com>
2023-08-23 08:41:02 +0900

    ビルドエラーが起きていたので修正

--- HappyPlaces/app/src/main/java/com/example/happyplaces/database/DatabaseHandler.kt
+++ HappyPlaces/app/src/main/java/com/example/happyplaces/database/DatabaseHandler.kt
@@ -27,14 +27,14 @@ class DatabaseHandler(context: Context) :
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val CREATE_HAPPY_PLACE_TABLE = ("CREATE_TABLE " + TABLE_HAPPY_PlACE + "(" +
-                + KEY_ID + " INTEGER PRIMARY KEY," +
-                + KEY_TITLE + " TEXT," +
-                + KEY_IMAGE + " TEXT," +
-                + KEY_DESCRIPTION + " TEXT," +
-                + KEY_DATE + " TEXT," +
-                + KEY_LOCATION + " TEXT," +
-                + KEY_LATITUDE + " TEXT," +
+        val CREATE_HAPPY_PLACE_TABLE = ("CREATE_TABLE " + TABLE_HAPPY_PlACE + "("
+                + KEY_ID + " INTEGER PRIMARY KEY,"
+                + KEY_TITLE + " TEXT,"
+                + KEY_IMAGE + " TEXT,"
+                + KEY_DESCRIPTION + " TEXT,"
+                + KEY_DATE + " TEXT,"
+                + KEY_LOCATION + " TEXT,"
+                + KEY_LATITUDE + " TEXT,"
                 + KEY_LONGITUDE + " TEXT)")
         db?.execSQL(CREATE_HAPPY_PLACE_TABLE)
 
