ae2a3a36dcd75a5db6a9756bbe275e7562f5ba3d
yosshi0127-ios <yosshi.0127.ios@gmail.com>
2023-11-04 16:50:05 +0900

    323. Changing The UI Depending On The Weather Data From The API

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -241,6 +241,25 @@ class MainActivity : AppCompatActivity() {
 
             binding.tvSunriseTime.text = unixTime(weatherResponse.sys.sunrise)
             binding.tvSunsetTime.text = unixTime(weatherResponse.sys.sunset)
+
+            when (weatherResponse.weather[i].icon) {
+                "01d" -> binding.ivMain.setImageResource(R.drawable.sunny)
+                "02d" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "03d" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "04d" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "04n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "10d" -> binding.ivMain.setImageResource(R.drawable.rain)
+                "11d" -> binding.ivMain.setImageResource(R.drawable.storm)
+                "13d" -> binding.ivMain.setImageResource(R.drawable.snowflake)
+                "01n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "02n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "03n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "10n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "11n" -> binding.ivMain.setImageResource(R.drawable.rain)
+                "13n" -> binding.ivMain.setImageResource(R.drawable.snowflake)
+            }
+
+
         }
     }
 
