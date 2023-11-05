41e05b3eaf3cb938604ffd3a7c1c3efed3bc5ddb
yosshi0127-ios <yosshi.0127.ios@gmail.com>
2023-11-05 16:03:23 +0900

    74. Day 5 - Button, onClick and Toast

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -35,27 +39,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
+
+    // SwiftUIでいうVStack?
     Column {
         // Here all the UI elements will be stacked bellow eatch other
+
+        Text("Unit Converter")
+
+        OutlinedTextField(value = "", onValueChange = {})
+
+        // SwiftUIでいうHStack?
         Row {
-            Greeting("Android")
-            Greeting("Android")
+            val context = LocalContext.current
+
+            Button(onClick = {
+                Toast.makeText(context, "Thanks for clicking!", Toast.LENGTH_LONG).show()
+
+            }) {
+                Text("Click Me!")
+            }
         }
+
+        Text("Result: ")
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     UnitConverterTheme {
-        Greeting("Android")
+        UnitConverter()
     }
 }
