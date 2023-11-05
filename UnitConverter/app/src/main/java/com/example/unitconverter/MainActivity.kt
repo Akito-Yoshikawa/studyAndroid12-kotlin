e43cc1e82c18427c390a1e79d1d6a3317414146e
yosshi0127-ios <yosshi.0127.ios@gmail.com>
2023-11-05 16:42:15 +0900

    76. Day 5 - Modifier, VerticalArrangement and HorizontalAlignment 77. Day 5 - Spacer vs Padding

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -17,10 +20,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,12 +51,20 @@ class MainActivity : ComponentActivity() {
 fun UnitConverter() {
 
     // SwiftUIでいうVStack?
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         // Here all the UI elements will be stacked bellow eatch other
 
         Text("Unit Converter")
 
+        Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(value = "", onValueChange = {})
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         // SwiftUIでいうHStack?
         Row {
@@ -72,6 +86,8 @@ fun UnitConverter() {
 
             }
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Text("Result: ")
 
