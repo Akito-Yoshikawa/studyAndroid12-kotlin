17ff1da63eb98dbc70cadcb10d76da6d7b7a7107
yosshi0127-ios <yosshi.0127.ios@gmail.com>
2023-11-15 23:18:36 +0900

    402. Preparing The Signup Feature in Firebase And In The App

--- Projemanag/app/src/main/java/com/example/projemanag/activitys/SignUpActivity.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/activitys/SignUpActivity.kt
@@ -3,12 +3,13 @@ package com.example.projemanag.activitys
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
 
@@ -43,6 +44,41 @@ class SignUpActivity : AppCompatActivity() {
 
         binding?.toolbarSignUpActivity?.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding?.btnSignUp?.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val name: String = binding?.etName?.text.toString().trim { it <= ' '}
+        val email: String = binding?.etEmail?.text.toString().trim { it <= ' '}
+        val password: String = binding?.etPassword?.text.toString().trim { it <= ' '}
+
+        if (validateForm(name, email, password)) {
+
+        }
+    }
+
+    /// Validationチェック
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter a email")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            } else -> {
+                true
+            }
         }
     }
 }
